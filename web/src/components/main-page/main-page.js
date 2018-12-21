d749e3ff68b5d4013c3e6d43be4a6522996c00c3
Miengha Behardien <miengha.behardien@krs.co.za>
2018-12-21 10:16:00 +0200

    Fixed issues with web and added test components

---
+++ web/src/components/main-page/main-page.js
@@ -0,0 +1,5 @@
+export class MainClass {
+  constructor() {
+    this.message = 'A message from the main class';
+  }
+}
---
+++ web/src/shared/routes/routes.js
@@ -0,0 +1,12 @@
+// import { PLATFORM } from 'aurelia-pal';
+
+// export class Routes {
+//   routes = [
+//     {
+//       name: 'main-page',
+//       route: ['', 'main-page'],
+//       moduleId: PLATFORM.moduleName('components/main-page/main-page'),
+//       title: 'MainPage'
+//     }
+//   ];
+// }
